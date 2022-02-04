5ac74dbb248a63802a1209703c996fa092a86248
MasterAddy <adesileisaiah70@gmail.com>
2022-02-04 10:31:34 -0800

    Chain Cert

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORTS = process.env.PORTS || 3030;
 app.use(express.json());
 
 const privateKey = fs.readFileSync(`${__dirname}/keys/key.pem`, "utf8");
-const certificate = fs.readFileSync(`${__dirname}/keys/live_trivoh_com.crt`, "utf8");
+const certificate = fs.readFileSync(`${__dirname}/keys/chain.crt`, "utf8");
 
 const credentials = { key: privateKey, cert: certificate };
 
